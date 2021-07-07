a2b64e89657f6231cfd124a600d839ec466eae5a
SaadAkbar <saadakbar@A006-00583.arbisoft.com>
2021-07-07 15:44:43 +0500

    Tasks pushed to my branch
    :wq
    
    :wq
    :wq
    :wq!
    :!wq

---
+++ task1.py
@@ -0,0 +1,6 @@
+squaredict = {}
+for i in range (1,16):
+    squaredict[i] = i*i
+
+print("Square Dict. The dictionary where value is key squared")
+print(squaredict)
---
+++ task2.py
@@ -0,0 +1,22 @@
+squaredict = {}
+quaddict = {}
+for i in range (1,16):
+    squaredict[i] = i*i
+
+for i in range (1,25):
+    quaddict[i] = i*i*i*i
+
+print("Square Dict. The dictionary where value is key squared")
+print(squaredict)
+print("Quad Dict. The dictionary where the value is key quadrupled (key^4)")
+print(quaddict)
+
+commondict = {}
+for i in range(1, 25):
+    try:
+        if (squaredict[i] and quaddict[i]):
+            commondict[i] = squaredict[i] + quaddict[i]
+    except:
+        continue
+print("Commondict. The dictionary of two added values")
+print(commondict)
