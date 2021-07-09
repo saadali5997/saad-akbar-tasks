b441e25548eda66eecadc9d64cd6d969c35f68e5
SaadAkbar <saadakbar@A006-00583.arbisoft.com>
2021-07-09 14:56:00 +0500

    Added a Unit test for task.py

--- task6.py
+++ task6.py
@@ -3,6 +3,7 @@ Task6. Reverse each word of a string passed as a script argument.
 '''
 
 import sys
+import unittest
 
 def main(argv):
     stringlist = argv[0].split(' ')
@@ -10,7 +11,11 @@ def main(argv):
     reversedlist = [word[::-1] for word in stringlist]
     
     reversedstring = reversedstring.join(reversedlist)
-    print(reversedstring)
+    return reversedstring
 
 if __name__ == "__main__":
    main(sys.argv[1:])
+
+class Testtask1(unittest.TestCase):
+    def test(self):
+        self.assertEqual(main(['asdfghjkl']),'lkjhgfdsa')
