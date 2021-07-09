d627649710e46cfb194443f3f9c807fd111aa017
SaadAkbar <saadakbar@A006-00583.arbisoft.com>
2021-07-09 12:13:27 +0500

    Added a unit test to task2.py

--- task2.py
+++ task2.py
@@ -2,25 +2,36 @@
 Task2. Add the common key values of 2 dictionaries.
 '''
 
-squaredict = {}
-quaddict = {}
-for i in range (1,16):
-    squaredict[i] = i*i
+import unittest
 
-for i in range (1,25):
-    quaddict[i] = i*i*i*i
+def main(range1low,range1high,range2low,range2high):
+    squaredict = {}
+    quaddict = {}
+    for i in range (range1low,range1high+1):
+        squaredict[i] = i*i
 
-print("Square Dict. The dictionary where value is key squared")
-print(squaredict)
-print("Quad Dict. The dictionary where the value is key quadrupled (key^4)")
-print(quaddict)
+    for i in range (range2low,range2high+1):
+        quaddict[i] = i*i*i*i
 
-commondict = {}
-for i in range(1, 25):
-    try:
-        if (squaredict[i] and quaddict[i]):
-            commondict[i] = squaredict[i] + quaddict[i]
-    except:
-        continue
-print("Commondict. The dictionary of two added values")
-print(commondict)
+    print("Square Dict. The dictionary where value is key squared")
+    print(squaredict)
+    print("Quad Dict. The dictionary where the value is key quadrupled (key^4)")
+    print(quaddict)
+
+    commondict = {}
+    for i in range(range2low, range2high):
+        try:
+            if (squaredict[i] and quaddict[i]):
+                commondict[i] = squaredict[i] + quaddict[i]
+        except:
+            continue
+    print("Commondict. The dictionary of two added values")
+    print(commondict)
+    return commondict
+if  __name__ == "__main__":
+    main(1,15,1,25)
+
+class Testtask1(unittest.TestCase):
+    def test(self):
+        dict = {1: 2, 2: 20, 3: 90, 4: 272, 5: 650, 6: 1332, 7: 2450, 8: 4160, 9: 6642, 10: 10100, 11: 14762, 12: 20880, 13: 28730, 14: 38612, 15: 50850}
+        self.assertEqual(main(1,15,1,25),dict)
