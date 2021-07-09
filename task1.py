f7acaace59fa6a8e0bb521187b82f10f40de8212
SaadAkbar <saadakbar@A006-00583.arbisoft.com>
2021-07-09 11:36:21 +0500

    Added a unit test to task1.py

--- task1.py
+++ task1.py
@@ -2,9 +2,22 @@
 Task1. Create a dictionary where value is key squared in the range(1,15) inclusive
 '''
 
-squaredict = {}
-for i in range (1,16):
-    squaredict[i] = i*i
+import unittest
 
-print("Square Dict. The dictionary where value is key squared")
-print(squaredict)
+def main():
+    squaredict = {}
+    for i in range (1,16):
+        squaredict[i] = i*i
+
+    print("Square Dict. The dictionary where value is key squared")
+    print(squaredict)
+    return squaredict
+if __name__ == '__main__':
+    main()
+
+
+class Testtask1(unittest.TestCase):
+    def test(self):
+        dict = {1: 1 ,2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225}
+        self.assertEqual(main(),dict)
+        
