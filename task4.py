00f2155abccead24a9b622df105a947241326f54
SaadAkbar <saadakbar@A006-00583.arbisoft.com>
2021-07-09 14:31:13 +0500

    Added a unit test for task4.py

--- task4.py
+++ task4.py
@@ -3,19 +3,27 @@ Task4. Check if a key exists in a dictionary or not
 '''
 
 import sys
+import unittest
 
 def main(argv):
     squaredict = {}
+
     for i in range(1,26):
         squaredict[i] = i*i
-    key = 9
+    try:
+        key = int(argv)
+    except:
+        key = int(argv[0])
+    print(key)
     
-    
-    if squaredict.has_key(key):
-        print("Key exists")
+    if key in squaredict:
+        return True
     else:
-        print("Key does not exist")
-
-if __name__ == "__main__": 
-    
+        return False
+        
+if __name__ == "__main__":     
     main(sys.argv[1:])
+
+class Testtask1(unittest.TestCase):
+    def test(self):
+        self.assertEqual(main(4),True)
